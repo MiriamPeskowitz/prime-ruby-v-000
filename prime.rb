87ad1f80f294037845896d0cc2e17a03cccbf8ea
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-22 00:38:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(num)
+
+
+      if num = num * 1 && num = num**2
+        true
+      elsif num = 1
+        false
+      elsif num == 2 || numb == 3
+        true
+      else
+        false
+      end
+
+
+end# Add  code here!
+
+=begin
+prime -- can no longer be divided by itself. whole number factors are 1 and itself
+2, 3, 5, 7, 11, 13, 17, 19, 23, and 29
+
+
+=end
